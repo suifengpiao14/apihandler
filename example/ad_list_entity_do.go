6e82684b2d16f8028d6f236627dbd61f65ee09a7
pz <2912150017@qq.com>
2023-02-23 21:35:51 +0800

    optimize

--- example/ad_list_entity_do.go
+++ example/ad_list_entity_do.go
@@ -6,13 +6,13 @@ import (
 	"github.com/suifengpiao14/controllerhandler"
 )
 
-func AdListDoFn(ctx context.Context, i AdListInput) (controllerhandler.OutputI, error) {
-	output := i.Output
+func AdListDoFn(ctx context.Context, input AdListInput) (controllerhandler.OutputI, error) {
+	output := input.Output
 	output.Code = "200"
 	output.Message = "ok"
 	output.Items = make([]AdListOutputItem, 0)
-	output.Pagination.Index = i.Index
-	output.Pagination.Size = i.Size
+	output.Pagination.Index = input.Index
+	output.Pagination.Size = input.Size
 	output.Pagination.Total = 100
 	item := AdListOutputItem{
 		ID: "1",
